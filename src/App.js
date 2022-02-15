b328516bc6c3324c1238b999deb4f1a85d4cb048
andreoneres <andreoliveiraneres@gmail.com>
2022-02-15 20:53:40 -0300

    data fetching and styles css

--- src/App.js
+++ src/App.js
@@ -4,30 +4,32 @@ import { Component } from "react";
 
 class App extends Component {
   state = {
-    posts: [
-      {
-        id: 1,
-        title: "O titulo 1",
-        body: "O corpo 1",
-      },
-      {
-        id: 2,
-        title: "O titulo 2",
-        body: "O corpo 2",
-      },
-      {
-        id: 3,
-        title: "O titulo 3",
-        body: "O corpo 3",
-      },
-    ],
+    posts: [],
     count: 1,
   };
 
   timeoutUpdate = null;
 
   componentDidMount() {
-    this.handleTimeout();
+    this.loadPosts();
+    // this.handleTimeout();
+  }
+
+  loadPosts = async() => {
+
+    const postsResponse = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const photosResponse = await fetch("https://jsonplaceholder.typicode.com/photos");
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url}
+    })
+
+    this.setState( { posts: postsAndPhotos} );
   }
 
   componentDidUpdate() {
@@ -52,16 +54,21 @@ class App extends Component {
     const { posts, count } = this.state;
 
     return (
-      <div className="App">
-        {posts.map((post) => (
-          <div key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-          </div>
-        ))}
-        <h1>{count}</h1>
-        <button>Somar</button>
-      </div>
+      <section className="container">
+        <div className="posts">
+          {posts.map((post) => (
+            <div key={post.id} className="post">
+              <img src={post.cover} alt="post-image"></img>
+              <div className="post-content">
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+              </div>
+            </div>
+          ))}
+          <h1>{count}</h1>
+          <button>Somar</button>
+        </div>
+      </section>
     );
   }
 }
