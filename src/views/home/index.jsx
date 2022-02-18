d1a1b4c9e3ab263d3c7ebe9325745471169ac285
andreoneres <andreoliveiraneres@gmail.com>
2022-02-17 21:58:58 -0300

    working with text input

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,16 +1,18 @@
-import "./styles.css";
+import "./styles.css"
 
 import { Component } from "react";
-import { Posts } from "../../components/posts";
-import { loadPosts } from "../../components/utils";
-import { Button } from "../../components/button";
+import { Posts } from "../../components/posts"
+import { loadPosts } from "../../components/utils"
+import { Button } from "../../components/button"
+import { TextInput } from "../../components/textInput";
 
 export class Home extends Component {
   state = {
     posts: [],
     allPosts: [],
     page: 0,
-    postsPerPage: 3
+    postsPerPage: 3,
+    searchValue: ''
   };
 
   async componentDidMount() {
@@ -36,20 +38,57 @@ export class Home extends Component {
     this.setState({ posts, page: nextPage })
   };
 
+
+  handleChange = (e) => {
+    const { value } = e.target
+    this.setState( { searchValue: value })
+  }
+
   render() {
-    const { posts, page, postsPerPage, allPosts } = this.state
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state
     const noMorePages = page + postsPerPage >= allPosts.length
+
+    const filteredPosts = !!searchValue ? allPosts.filter(post => {
+      return post.title.toLowerCase().includes(searchValue.toLowerCase())
+    }) 
+    : 
+    posts
 
     return (
       <section className="container">
-        <Posts posts={posts} />
-        <div className="button-container">
-          <Button
-            text="Load more images..." 
-            onClick={this.loadMorePosts} 
-            disabled={noMorePages}
+       
+        <div className="search-container">
+          { !!searchValue && (
+            <>
+              <h2>Search value: {searchValue}</h2><br/>
+            </>
+          )}
+
+          <TextInput
+            searchValue={searchValue}
+            handleChange={this.handleChange}
           />
         </div>
+
+        { filteredPosts.length > 0 && (
+          <Posts posts={filteredPosts} />
+        )}
+
+        { filteredPosts.length === 0 && (
+          <p>Não existem posts =(</p>
+        )}
+
+          <>
+            <div className="button-container">
+            { !searchValue && (
+              <Button
+                text="Load more images..." 
+                onClick={this.loadMorePosts} 
+                disabled={noMorePages}
+              />
+            )}
+            </div>
+          </>
       </section>
     );
   }
